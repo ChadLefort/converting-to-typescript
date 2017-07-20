80b15db21130b36e9a3caea8cf15e2a64e793b63
Chad Lefort <chadl@pecsafety.com>
2017-07-20 15:39:28 -0500

    Added redux devtools.

--- src/store.ts
+++ src/store.ts
@@ -1,5 +1,6 @@
 import { applyMiddleware, createStore } from 'redux';
 import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import { rootReducer } from './reducers';
 
-export default createStore(rootReducer, applyMiddleware(thunk));
+export default createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
